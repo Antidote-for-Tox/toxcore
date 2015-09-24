c1d83cbe73f36c7286bb1c85179a4ba4c81b7f56
Dmytro Vorobiov <d@dvor.me>
2015-09-24 23:38:35 +0200

    Rebuilding vpx for both iOS and OS X

---
+++ osx/vpx.framework/Headers/vpx_config.h
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2015 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+/* GENERATED FILE: DO NOT EDIT! */
+
+#ifndef VPX_FRAMEWORK_HEADERS_VPX_VPX_CONFIG_H_
+#define VPX_FRAMEWORK_HEADERS_VPX_VPX_CONFIG_H_
+
+#if defined __x86_64__
+#define VPX_FRAMEWORK_TARGET "x86_64-darwin13-gcc"
+#include "vpx/vpx/x86_64-darwin13-gcc/vpx_config.h"
+#endif
+
+#endif  // VPX_FRAMEWORK_HEADERS_VPX_VPX_CONFIG_H_
